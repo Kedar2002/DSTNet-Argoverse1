1e219fc5e9c0204d2731dda8205905c135d2b8d7
Kedar <kedaradhikari2002@gmail.com>
2026-08-05 09:41:34 +0530

    feat(multiple): General update

--- scripts/train_kaggle.py
+++ scripts/train_kaggle.py
@@ -122,7 +122,7 @@ TEST_ROOT = (
 )
 
 MAP_ROOT = (
-    "/kaggle/input/datasets/kedaradhikari/argoverse1-hdmaps/"
+    "/kaggle/input/datasets/kedaradhikari/argoverse1-hdmapss/"
     "hd_maps/map_files"
 )
 
